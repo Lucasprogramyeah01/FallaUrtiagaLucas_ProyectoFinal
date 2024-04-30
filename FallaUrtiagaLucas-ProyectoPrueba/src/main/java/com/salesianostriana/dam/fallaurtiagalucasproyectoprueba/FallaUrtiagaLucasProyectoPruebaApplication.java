fa4e5f33c3ba80fc150fddc8f0b0b7aaec367e3d
LucasFallaUrtiaga <falla.urluc24_triana.salesianos.edu>
2024-04-30 12:33:29 +0200

    Rama en problemas

---
+++ FallaUrtiagaLucas-ProyectoPrueba/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectoprueba/FallaUrtiagaLucasProyectoPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.fallaurtiagalucasproyectoprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallaUrtiagaLucasProyectoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallaUrtiagaLucasProyectoPruebaApplication.class, args);
+	}
+
+}
