d9efed88677c803afc6e4b5e03d5a6b94fa1bed8
Lucas <falla.urluc24@triana.salesianos.edu>
2024-05-05 22:27:35 +0200

    Todo arreglado (creo)

---
+++ FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/FallaUrtiagaLucasProyectoFinal1DamApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallaUrtiagaLucasProyectoFinal1DamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallaUrtiagaLucasProyectoFinal1DamApplication.class, args);
+	}
+
+}
