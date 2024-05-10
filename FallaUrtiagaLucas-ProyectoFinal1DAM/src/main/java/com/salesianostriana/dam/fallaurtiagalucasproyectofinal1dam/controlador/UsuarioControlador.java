104f293e5b426a89706cf8f2849089a9aee1008b
LucasFallaUrtiaga <falla.urluc24_triana.salesianos.edu>
2024-05-10 14:27:21 +0200

    Oh yeah

--- FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectofinal1dam/controlador/UsuarioControlador.java
+++ FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectofinal1dam/controlador/UsuarioControlador.java
@@ -3,16 +3,18 @@ package com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.controlador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.servicio.UsuarioServicio;
 
 @Controller
+@RequestMapping("/admin")
 public class UsuarioControlador {
 
 	@Autowired
 	private UsuarioServicio servicio;
 	
-	@GetMapping("/admin/clientes")
+	@GetMapping("/clientes")
 	public String mostrarInicio() {
 		return "admin/pagAdminClientes";
 	}
