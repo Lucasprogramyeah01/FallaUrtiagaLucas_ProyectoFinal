b368b2c8dd69ae3977cf89ce352161bddbd1cac4
Lucas <falla.urluc24@triana.salesianos.edu>
2024-05-15 23:26:53 +0200

    Intento de Enum funcional en Thymeleaf.

--- FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectofinal1dam/modelo/Cubierta.java
+++ FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectofinal1dam/modelo/Cubierta.java
@@ -1,7 +1,17 @@
 package com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.modelo;
 
 public enum Cubierta {
-	TapaBlanda,
-	TapaDura,
-	Sobrecubierta;
+	TapaBlanda("Tapa blanda"),
+	TapaDura("Tapa dura"),
+	Sobrecubierta("Sobrecubierta");
+	
+	private final String displayValue;
+	
+	private Cubierta (String displayValue) {
+		this.displayValue = displayValue;
+	}
+	
+	public String getDisplayValue() {
+		return displayValue;
+	}
 }
