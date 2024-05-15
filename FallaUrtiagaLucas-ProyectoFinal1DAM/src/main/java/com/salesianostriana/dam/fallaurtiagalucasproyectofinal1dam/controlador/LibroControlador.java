efb8d9288441cc76342e042adfb80fd0ddd7df9b
LucasFallaUrtiaga <falla.urluc24_triana.salesianos.edu>
2024-05-15 14:28:45 +0200

    Import.sql creado, categorias introducidas, crud categorias medio funcionan

--- FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectofinal1dam/controlador/LibroControlador.java
+++ FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/java/com/salesianostriana/dam/fallaurtiagalucasproyectofinal1dam/controlador/LibroControlador.java
@@ -1,15 +1,20 @@
 package com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.controlador;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.modelo.Categoria;
 import com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.modelo.Libro;
+import com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.modelo.Usuario;
+import com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.servicio.CategoriaServicio;
 import com.salesianostriana.dam.fallaurtiagalucasproyectofinal1dam.servicio.LibroServicio;
 
 @Controller
@@ -19,47 +24,70 @@ public class LibroControlador {
 	@Autowired
 	private LibroServicio servicio;
 	
+	@Autowired
+	private CategoriaServicio servicioCat;
+	
 	//MOSTRAR LISTA DE LIBROS.
 	@GetMapping("/listaLibros")
 	public String mostrarListaLibros(Model model) {
-		model.addAttribute("listaLibros", servicio.findAll());
+		model.addAttribute("listaLibros", servicio.findAll());	//CUIDAO
 		
 		return "/admin/pagAdminProductos";
 	}
 	
-	//MOSTRAR FORMULARIO PARA AGREGAR CATEGORIA.
+	//MOSTRAR FORMULARIO PARA AGREGAR LIBRO.
 	@GetMapping("/agregarLibro")
 	public String mostrarFormularioLibro(Model model) {
 			
 		Libro l = new Libro();
 		model.addAttribute("libro", l);
+		model.addAttribute("listaCategorias", servicioCat.findAll());	//CUIDAO
 				
 		return "/admin/pagAdminAgregarProducto";
 	}
 	
-	//AGREGAR CATEGORÍA.
+	//AGREGAR LIBRO.
 	@PostMapping("/agregarLibro/submit")
-	public String procesarFormularioLibros(@ModelAttribute("libro") Libro l, Model model) {
+	public String procesarFormularioLibro(@ModelAttribute("libro") Libro l, Model model) {
 		servicio.save(l);
-				
+		model.addAttribute("listaCategorias", servicioCat.findAll());	//CUIDAO
+		
 		return "redirect:/admin/listaLibros";
 	}
 	
+	//MOSTRAR FORMULARIO PARA EDITAR LIBRO.
+	@GetMapping("/editarLibro/{id}")
+	public String mostrarFormularioEditarLibro(@PathVariable("id") long id, Model model) {
+		model.addAttribute("listaCategorias", servicioCat.findAll());	//CUIDAO
+			
+		Optional<Libro> libro = servicio.findById(id);
+			
+		if (libro.isPresent()) {
+			model.addAttribute("libro", libro.get());
+			model.addAttribute("listaCategorias", servicioCat.findAll());	//CUIDAO
+			model.addAttribute("libro", libro);		//CUIDAO
+				
+			return "/admin/pagAdminAgregarProducto";
+		}else {
+				
+			return "redirect:/admin/listaLibros";
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//EDITAR LIBRO.
+	@PostMapping("/editarLibro/submit")
+	public String procesarFormularioEditarLibro(@ModelAttribute("libro") Libro l) {
+		servicio.save(l);
+			
+		return "redirect:/admin/listaLibros";
+	}
+		
+	//BORRAR LIBRO.
+	@GetMapping("/borrarLibro/{id}")
+	public String borrarLibro(@PathVariable("id") long idLibro) {
+		servicio.deleteById(idLibro);
+			
+		return "redirect:/admin/listaLibros";
+	}
 	
 }
---
+++ FallaUrtiagaLucas-ProyectoFinal1DAM/src/main/resources/import.sql
@@ -0,0 +1,27 @@
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (1, null,'Aventuras');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (2, null,'Ciencia ficción');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (3, null,'Romance');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (4, null,'Misterio');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (5, null,'Terror');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (6, null,'Comedia');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (7, null,'Acción');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (8, null,'Policíaco');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (9, null,'Fantasía');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (10, null, 'Gore');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (11, null, 'Historia');
+INSERT INTO Categoria(id_categoria, libro_id_libro, nombre) VALUES (12, null, 'Lírica');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
